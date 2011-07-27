58cdc3495eddaf825062cf7a885a9fda36322061
Roman <roman@burningswell.com>
2011-07-27 20:53:11 +0200

    Moved keyboad bindings to bindings.el.

--- init.el
+++ init.el
@@ -304,22 +304,6 @@ server."
 (require 'dropdown-list)
 (setq yas/prompt-functions '(yas/dropdown-prompt yas/ido-prompt yas/completing-prompt))
 
-;; KEY BINDINGS
-
-;; Restore some default key bindings overridden by emacs starter kit.
-(global-set-key (kbd "C-x h") 'mark-whole-buffer)
-(global-set-key (kbd "C-x ^") 'enlarge-window)
-(global-set-key (kbd "C-x C-o") 'delete-blank-lines)
-(global-unset-key (kbd "C-x g"))
-
-(global-set-key (kbd "C-c C-n") 'durendal-sort-ns)
-(global-set-key (kbd "C-c C-s") 'swap-windows)
-(global-set-key (kbd "C-x C-g b") 'mo-git-blame-current)
-(global-set-key (kbd "C-x C-g s") 'magit-status)
-(global-set-key (kbd "C-x I") 'indent-buffer)
-(global-set-key (kbd "C-x TAB") 'indent-rigidly)
-(global-set-key [f5] 'compile)
-
 ;; Jump into eshell
 (eshell)
 
---
+++ roman/bindings.el
@@ -0,0 +1,13 @@
+;; Restore some default key bindings overridden by emacs starter kit.
+(global-set-key (kbd "C-x h") 'mark-whole-buffer)
+(global-set-key (kbd "C-x ^") 'enlarge-window)
+(global-set-key (kbd "C-x C-o") 'delete-blank-lines)
+(global-unset-key (kbd "C-x g"))
+
+(global-set-key (kbd "C-c C-n") 'durendal-sort-ns)
+(global-set-key (kbd "C-c C-s") 'swap-windows)
+(global-set-key (kbd "C-x C-g b") 'mo-git-blame-current)
+(global-set-key (kbd "C-x C-g s") 'magit-status)
+(global-set-key (kbd "C-x I") 'indent-buffer)
+(global-set-key (kbd "C-x TAB") 'indent-rigidly)
+(global-set-key [f5] 'compile)
