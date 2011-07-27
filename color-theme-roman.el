42a66527baedc243fc2baf175a1f6960874bbffc
Roman <roman@burningswell.com>
2011-07-27 21:49:44 +0200

    Added provide to custom color theme.

--- color-theme-roman.el
+++ color-theme-roman.el
@@ -1,13 +1,14 @@
-(eval-when-compile (require 'color-theme))
+(require 'color-theme)
+
 (defun color-theme-roman ()
   "Color theme by Roman Scherer."
   (interactive)
   (color-theme-install
    '(color-theme-roman
-     (
-      )
      ()
-     (mode-line ((t (:background "black" :foreground "white" :box (:line-width -1 :style released-button)))))
-   )))
+     ()
+     (mode-line ((t (:background "black" :foreground "white" :box (:line-width -1 :style released-button))))))))
 
 (add-to-list 'color-themes '(color-theme-roman  "Roman" "Roman Scherer"))
+
+(provide 'color-theme-roman)
