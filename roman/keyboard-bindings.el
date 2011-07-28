f8c6134f84e5dd39c20c45a61240529e1f70df77
Roman Scherer <roman.scherer@nugg.ad>
2011-07-28 10:26:11 +0200

    Added keyboard binding for find-file-in-project.

--- roman/keyboard-bindings.el
+++ roman/keyboard-bindings.el
@@ -1,11 +1,14 @@
-;; Restore some default key bindings overridden by emacs starter kit.
+;; Restore some default key bindings that are overridden by the emacs
+;; starter kit.
 (global-set-key (kbd "C-x h") 'mark-whole-buffer)
 (global-set-key (kbd "C-x ^") 'enlarge-window)
 (global-set-key (kbd "C-x C-o") 'delete-blank-lines)
 (global-unset-key (kbd "C-x g"))
 
+;; My keboard bindings.
 (global-set-key (kbd "C-c C-n") 'durendal-sort-ns)
 (global-set-key (kbd "C-c C-s") 'swap-windows)
+(global-set-key (kbd "C-x C-M-f") 'find-file-in-project)
 (global-set-key (kbd "C-x C-g b") 'mo-git-blame-current)
 (global-set-key (kbd "C-x C-g s") 'magit-status)
 (global-set-key (kbd "C-x I") 'indent-buffer)
