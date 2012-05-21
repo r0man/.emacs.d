212f35dcfe7e5e6b65afa795d0877eb604050751
Roman Scherer <roman.scherer@nugg.ad>
2012-05-21 14:47:29 +0200

    Use bundle exec to run ruby specs.

--- init.el
+++ init.el
@@ -372,6 +372,8 @@ new one."
 (require 'ruby-test-mode)
 (setq ruby-test-ruby-executables '("/usr/local/rvm/rubies/ruby-1.9.2-p180/bin/ruby")
       ruby-test-rspec-executables '("bundle exec rspec"))
+(setq ruby-test-ruby-executables '("/usr/local/rvm/rubies/ruby-1.9.3-p194/bin/ruby")
+      ruby-test-rspec-executables '("bundle exec rspec"))
 
 ;; EMACS RAILS RELOADED
 (setq rails/rspec-bundle/command "bundle exec rspec")
