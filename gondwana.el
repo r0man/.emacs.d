041f45e2d177bb13603dba6c0b6c532c382ca0b1
Roman Scherer <roman.scherer@nugg.ad>
2012-06-11 12:26:33 +0200

    Set mail account.

--- gondwana.el
+++ gondwana.el
@@ -1,2 +1,5 @@
 ;; Full mailing address of this user.
 (setq user-mail-address "roman.scherer@nugg.ad")
+
+;; Tell msmtp which account to use.
+(setq message-sendmail-extra-arguments '("-a" "roman.scherer@nugg.ad"))
