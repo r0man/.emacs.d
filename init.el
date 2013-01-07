8576301ea6eb1936c614dbb414e30de9b58f5840
Roman Scherer <roman.scherer@burningswell.com>
2013-01-07 20:07:52 +0100

    Better swap-windows.

--- init.el
+++ init.el
@@ -91,21 +91,28 @@
         (setenv "EC2_CERT" (expand-file-name ec2-cert)))))
 
 (defun swap-windows ()
-  "If you have 2 windows, it swaps them."
-  (interactive) (cond ((/= (count-windows) 2)
-         (message "You need exactly 2 windows to do this."))
+  "Swap your windows"
+  (interactive)
+  (cond ((not (> (count-windows)1))
+         (message "You can't rotate a single window!"))
         (t
-         (let* ((w1 (first (window-list)))
-                (w2 (second (window-list)))
-                (b1 (window-buffer w1))
-                (b2 (window-buffer w2))
-                (s1 (window-start w1))
-                (s2 (window-start w2)))
-           (set-window-buffer w1 b2)
-           (set-window-buffer w2 b1)
-           (set-window-start w1 s2)
-           (set-window-start w2 s1))))
-  (other-window 1))
+         (setq i 1)
+         (setq numWindows (count-windows))
+         (while  (< i numWindows)
+           (let* (
+                  (w1 (elt (window-list) i))
+                  (w2 (elt (window-list) (+ (% i numWindows) 1)))
+
+                  (b1 (window-buffer w1))
+                  (b2 (window-buffer w2))
+
+                  (s1 (window-start w1))
+                  (s2 (window-start w2)))
+             (set-window-buffer w1  b2)
+             (set-window-buffer w2 b1)
+             (set-window-start w1 s2)
+             (set-window-start w2 s1)
+             (setq i (1+ i)))))))
 
 ;; This variable describes the behavior of the command key.
 (setq mac-option-key-is-meta t)
@@ -188,7 +195,7 @@
 
 ;; CLOJURESCRIPT
 (add-to-list 'auto-mode-alist '("\\.cljs$" . clojure-mode))
-(setq inferior-lisp-program "lein trampoline cljsbuild repl-launch chromium")
+(setq inferior-lisp-program "lein trampoline noderepl")
 
 (defun lein-cljs-build ()
   (interactive)
