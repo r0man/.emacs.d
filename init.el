a56285a5a703b77b3a2cbcd6b3427042258ff493
Roman Scherer <roman.scherer@burningswell.com>
2013-01-08 19:44:05 +0100

    Added toggle-window-split.

--- init.el
+++ init.el
@@ -114,6 +114,31 @@
              (set-window-start w2 s1)
              (setq i (1+ i)))))))
 
+(defun toggle-window-split ()
+  (interactive)
+  (if (= (count-windows) 2)
+      (let* ((this-win-buffer (window-buffer))
+             (next-win-buffer (window-buffer (next-window)))
+             (this-win-edges (window-edges (selected-window)))
+             (next-win-edges (window-edges (next-window)))
+             (this-win-2nd (not (and (<= (car this-win-edges)
+                                         (car next-win-edges))
+                                     (<= (cadr this-win-edges)
+                                         (cadr next-win-edges)))))
+             (splitter
+              (if (= (car this-win-edges)
+                     (car (window-edges (next-window))))
+                  'split-window-horizontally
+                'split-window-vertically)))
+        (delete-other-windows)
+        (let ((first-win (selected-window)))
+          (funcall splitter)
+          (if this-win-2nd (other-window 1))
+          (set-window-buffer (selected-window) this-win-buffer)
+          (set-window-buffer (next-window) next-win-buffer)
+          (select-window first-win)
+          (if this-win-2nd (other-window 1))))))
+
 ;; This variable describes the behavior of the command key.
 (setq mac-option-key-is-meta t)
 (setq mac-right-option-modifier nil)
