faf331fc7bcc0c6184cd9420ac20a7d807161fd6
Roman Scherer <roman.scherer@burningswell.com>
2013-02-22 10:40:07 +0100

    Added rotate-windows.

--- init.el
+++ init.el
@@ -99,30 +99,6 @@
         (setenv "EC2_PRIVATE_KEY" (expand-file-name ec2-private-key))
         (setenv "EC2_CERT" (expand-file-name ec2-cert)))))
 
-(defun swap-windows ()
-  "Swap your windows"
-  (interactive)
-  (cond ((not (> (count-windows)1))
-         (message "You can't rotate a single window!"))
-        (t
-         (setq i 1)
-         (setq numWindows (count-windows))
-         (while  (< i numWindows)
-           (let* (
-                  (w1 (elt (window-list) i))
-                  (w2 (elt (window-list) (+ (% i numWindows) 1)))
-
-                  (b1 (window-buffer w1))
-                  (b2 (window-buffer w2))
-
-                  (s1 (window-start w1))
-                  (s2 (window-start w2)))
-             (set-window-buffer w1  b2)
-             (set-window-buffer w2 b1)
-             (set-window-start w1 s2)
-             (set-window-start w2 s1)
-             (setq i (1+ i)))))))
-
 (defun compass-watch ()
   "Find the project root and run compass watch."
   (interactive)
@@ -133,7 +109,29 @@
         (compile (message (format "cd %s; compass watch" directory)))
       (message "Can't find compass project root."))))
 
-(defun toggle-window-split ()
+(defun swap-windows ()
+  "Swap your windows."
+  (interactive)
+  (cond ((not (> (count-windows)1))
+         (message "You can't rotate a single window!"))
+        (t
+         (setq i 1)
+         (setq numWindows (count-windows))
+         (while  (< i numWindows)
+           (let* ((w1 (elt (window-list) i))
+                  (w2 (elt (window-list) (+ (% i numWindows) 1)))
+                  (b1 (window-buffer w1))
+                  (b2 (window-buffer w2))
+                  (s1 (window-start w1))
+                  (s2 (window-start w2)))
+             (set-window-buffer w1  b2)
+             (set-window-buffer w2 b1)
+             (set-window-start w1 s2)
+             (set-window-start w2 s1)
+             (setq i (1+ i)))))))
+
+(defun rotate-windows ()
+  "Rotate your windows."
   (interactive)
   (if (= (count-windows) 2)
       (let* ((this-win-buffer (window-buffer))
