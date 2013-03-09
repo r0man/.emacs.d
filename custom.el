9a699395fba6f6b02e6d200918f491f8c069e366
Roman Scherer <roman.scherer@burningswell.com>
2013-03-09 22:11:38 +0100

    Customized modeline.

--- custom.el
+++ custom.el
@@ -3,10 +3,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(send-mail-function (quote sendmail-send-it)))
+ )
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(mode-line ((t (:background "brightgreen" :foreground "black" :inverse-video t :box nil :underline nil :slant normal :weight normal)))))
