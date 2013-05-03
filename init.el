f6778fc9ba44ecc5275a3d0db05b9da7849dc5e1
Roman Scherer <roman.scherer@burningswell.com>
2013-05-03 23:14:50 +0200

    Init ruby test mode via el-get.

--- init.el
+++ init.el
@@ -512,13 +512,6 @@ new one."
    (when (file-exists-p "/usr/local/rvm")
      (rvm-use-default))
 
-   ;; RUBY-TEST-MODE
-   (require 'ruby-test-mode)
-   (setq ruby-test-ruby-executables '("/usr/local/rvm/rubies/ruby-1.9.2-p180/bin/ruby")
-	 ruby-test-rspec-executables '("bundle exec rspec"))
-   (setq ruby-test-ruby-executables '("/usr/local/rvm/rubies/ruby-1.9.3-p194/bin/ruby")
-	 ruby-test-rspec-executables '("bundle exec rspec"))
-
    ;; WINNER-MODE
    (winner-mode)
 
