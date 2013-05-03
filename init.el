82c2b884978672743f859e5b3d666dad95a51fa6
Roman Scherer <roman.scherer@burningswell.com>
2013-05-03 23:11:22 +0200

    Init haskell via el-get.

--- init.el
+++ init.el
@@ -345,12 +345,6 @@
 ;; Initialize the Gnus daemon.
 (gnus-demon-init)
 
-;; HASKELL-MODE
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
-
 ;; JAVA
 
 ;; Indent Java annotations.
