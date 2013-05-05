b6c5e08109738ca53832de01f45544d2dd39cd83
Roman Scherer <roman.scherer@burningswell.com>
2013-05-05 14:07:50 +0200

    Use eval-after-load for define-clojure-indent.

--- el-get-init-files/init-clojure-mode.el
+++ el-get-init-files/init-clojure-mode.el
@@ -1,27 +1,30 @@
 (add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
 (add-to-list 'auto-mode-alist '("\\.cljs$" . clojure-mode))
 
-(add-hook 'clojure-mode-hook
-          (lambda ()
-            (define-clojure-indent
-              (ANY 2)
-              (DELETE 2)
-              (GET 2)
-              (HEAD 2)
-              (POST 2)
-              (PUT 2)
-              (domonad 1)
-              (context 2)
-              (api-test 1)
-              (web-test 1)
-              (database-test 1)
-              (defroutes 'defun)
-              ;; SQLingvo
-              (copy 2)
-              (create-table 1)
-              (delete 1)
-              (drop-table 1)
-              (insert 2)
-              (select 1)
-              (truncate 1)
-              (update 2))))
+(eval-after-load "clojure-mode"
+  '(define-clojure-indent
+     ;; COMPOJURE
+     (ANY 2)
+     (DELETE 2)
+     (GET 2)
+     (HEAD 2)
+     (POST 2)
+     (PUT 2)
+     (context 2)
+     ;; ALGO.MONADS
+     (domonad 1)
+     ;; SQLINGVO
+     (copy 2)
+     (create-table 1)
+     (delete 1)
+     (drop-table 1)
+     (insert 2)
+     (select 1)
+     (truncate 1)
+     (update 2)
+     ;; CUSTOM
+     (api-test 1)
+     (web-test 1)
+     (database-test 1)
+     (defroutes 'defun)
+     ))
