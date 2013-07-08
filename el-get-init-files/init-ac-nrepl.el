ad0f82ddb9d85c1b51c7d68ce2c9509f800c149a
Roman Scherer <roman.scherer@burningswell.com>
2013-07-08 18:47:02 +0200

    Configured ac-nrepl.

--- el-get-init-files/init-ac-nrepl.el
+++ el-get-init-files/init-ac-nrepl.el
@@ -1,1 +1,6 @@
+(add-hook 'nrepl-mode-hook 'ac-nrepl-setup)
+(add-hook 'nrepl-interaction-mode-hook 'ac-nrepl-setup)
+(eval-after-load "auto-complete" '(add-to-list 'ac-modes 'nrepl-mode))
+
+;; Use ac-nrepl's popup documentation in place of nrepl-doc
 (define-key nrepl-interaction-mode-map (kbd "C-c C-d") 'ac-nrepl-popup-doc)
