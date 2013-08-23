5404fb705dbc07c64ec748f9eeefc283c610a23f
Roman Scherer <roman.scherer@burningswell.com>
2013-08-23 15:35:16 +0200

    Removed font setting.

--- custom.el
+++ custom.el
@@ -13,7 +13,6 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Terminus" :foundry "xos4" :slant normal :weight normal :height 105 :width normal))))
  '(cursor ((t (:background "orange red" :foreground "#141b23"))))
  '(mode-line ((t (:background "white smoke" :foreground "black"))))
  '(mode-line-inactive ((t (:background "dark gray" :foreground "black"))))
