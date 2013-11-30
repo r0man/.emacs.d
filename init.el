20420a0121d3965967663468df7e800a95606e5c
Roman Scherer <roman.scherer@burningswell.com>
2013-11-30 11:45:26 +0100

    Bind C-x C-d to dired.

--- init.el
+++ init.el
@@ -522,9 +522,11 @@
    (global-set-key (kbd "C-c SPC") 'ace-jump-mode)
    (global-set-key (kbd "C-c n") 'cleanup-buffer)
    (global-set-key (kbd "C-x C-g b") 'mo-git-blame-current)
+   (global-set-key (kbd "C-x C-d") 'dired)
    (global-set-key (kbd "C-x C-o") 'delete-blank-lines)
    (global-set-key (kbd "C-x TAB") 'indent-rigidly)
    (global-set-key (kbd "C-x ^") 'enlarge-window)
+   (global-set-key (kbd "C-x f") 'projectile-find-file)
    (global-set-key (kbd "C-x h") 'mark-whole-buffer)
    (global-set-key (kbd "C-c r") 'rotate-buffers)
 
