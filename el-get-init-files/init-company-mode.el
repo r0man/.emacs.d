8b4d0decb5fe49f8eafcd020279339f83ccd0d10
Roman Scherer <roman.scherer@burningswell.com>
2014-04-21 20:15:51 +0200

    Add nicer colors for company-mode.

--- el-get-init-files/init-company-mode.el
+++ el-get-init-files/init-company-mode.el
@@ -1,3 +1,13 @@
+(require 'color)
+
+(let ((bg (face-attribute 'default :background)))
+  (custom-set-faces
+   `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
+   `(company-tooltip-selection ((t (:inherit default :background ,(color-lighten-name bg 6)))))
+   `(company-scrollbar-bg ((t (:inherit default :background ,(color-lighten-name bg 4)))))
+   `(company-scrollbar-fg ((t (:inherit default :background ,(color-lighten-name bg 8)))))
+   `(company-tooltip-common ((t (:inherit default :background ,bg))))
+   `(company-tooltip-common-selection ((t (:inherit default :background ,(color-lighten-name bg 8)))))))
 
 (defun indent-or-complete ()
   (interactive)
@@ -6,4 +16,5 @@
     (indent-according-to-mode)))
 
 (global-set-key (kbd "TAB") 'indent-or-complete)
+
 (add-hook 'after-init-hook 'global-company-mode)
