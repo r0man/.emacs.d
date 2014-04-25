9f5d47a06bcff707715c78e2770bc960cae07624
Roman Scherer <roman.scherer@burningswell.com>
2014-04-25 15:23:00 +0200

    Custom company-mode colors.

--- el-get-init-files/init-company-mode.el
+++ el-get-init-files/init-company-mode.el
@@ -1,20 +1,31 @@
-(require 'color)
+(require 'company)
 
-(let ((bg (face-attribute 'default :background)))
-  (custom-set-faces
-   `(company-tooltip ((t (:inherit default :background ,(color-lighten-name bg 2)))))
-   `(company-tooltip-selection ((t (:inherit default :background ,(color-lighten-name bg 6)))))
-   `(company-scrollbar-bg ((t (:inherit default :background ,(color-lighten-name bg 4)))))
-   `(company-scrollbar-fg ((t (:inherit default :background ,(color-lighten-name bg 8)))))
-   `(company-tooltip-common ((t (:inherit default :background ,bg))))
-   `(company-tooltip-common-selection ((t (:inherit default :background ,(color-lighten-name bg 8)))))))
+(custom-set-faces
+ '(company-preview
+   ((t (:foreground "darkgray" :underline t))))
+ '(company-preview-common
+   ((t (:inherit company-preview))))
+ '(company-tooltip
+   ((t (:background "lightgray" :foreground "black"))))
+ '(company-tooltip-selection
+   ((t (:background "steelblue" :foreground "white"))))
+ '(company-tooltip-common
+   ((((type x)) (:inherit company-tooltip :weight bold))
+    (t (:inherit company-tooltip))))
+ '(company-tooltip-common-selection
+   ((((type x)) (:inherit company-tooltip-selection :weight bold))
+    (t (:inherit company-tooltip-selection)))))
+
+(let ((mode company-mode-map))
+  (define-key mode (kbd "C-n") 'company-select-next)
+  (define-key mode (kbd "C-p") 'company-select-previous))
 
 (defun indent-or-complete ()
+  "Indent or complete via company-mode."
   (interactive)
   (if (looking-at "\\_>")
       (company-complete-common)
     (indent-according-to-mode)))
 
 (global-set-key (kbd "TAB") 'indent-or-complete)
-
-(add-hook 'after-init-hook 'global-company-mode)
+(global-company-mode)
