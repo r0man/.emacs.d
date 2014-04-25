3eb19a050030280745b336c7156be82e00368a5f
Roman Scherer <roman.scherer@burningswell.com>
2014-04-25 23:19:33 +0200

    Remove bombaclaat.el

--- bombaclaat.el
+++
@@ -1,5 +0,0 @@
-;; Full mailing address of this user.
-(setq user-mail-address "roman.scherer@burningswell.com")
-
-;; Local domain name without a host name.
-(setq smtpmail-local-domain "burningswell.com")
