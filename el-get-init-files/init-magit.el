53547ecebe8e2cc11c0b0a30d52fd8198d109e7d
Roman Scherer <roman.scherer@burningswell.com>
2014-05-02 15:41:02 +0200

    Load magit via use-package

--- el-get-init-files/init-magit.el
+++
@@ -1,4 +0,0 @@
-(global-set-key (kbd "C-x C-g s") 'magit-status)
-(setq magit-stage-all-confirm nil)
-(setq magit-unstage-all-confirm nil)
-(setq ediff-window-setup-function 'ediff-setup-windows-plain)
