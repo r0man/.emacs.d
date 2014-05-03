0c9f2286f723caecad707e91dcbac6b5e4947751
Roman Scherer <roman.scherer@burningswell.com>
2014-05-03 20:42:42 +0200

    Move faces to custom.el

--- custom.el
+++ custom.el
@@ -14,4 +14,16 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- )
+ '(company-preview ((t (:foreground "darkgray" :underline t))))
+ '(company-preview-common ((t (:inherit company-preview))))
+ '(company-scrollbar-bg ((t (:background "darkgray"))))
+ '(company-scrollbar-fg ((t (:background "black"))))
+ '(company-tooltip ((t (:background "lightgray" :foreground "black"))))
+ '(company-tooltip-common ((((type x)) (:inherit company-tooltip :weight bold)) (t (:inherit company-tooltip))))
+ '(company-tooltip-common-selection ((((type x)) (:inherit company-tooltip-selection :weight bold)) (t (:inherit company-tooltip-selection))))
+ '(company-tooltip-selection ((t (:background "steelblue" :foreground "white"))))
+ '(cursor ((t (:background "orange red" :foreground "white"))))
+ '(magit-section-title ((t (:underline t))))
+ '(mode-line ((t (:background "white smoke" :foreground "black"))))
+ '(mode-line-inactive ((t (:background "dark gray" :foreground "black"))))
+ '(vertical-border ((t (:foreground "black")))))
