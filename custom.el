389dfea86da8f8ef080c5c92cb1eb9b7fa4fb342
Roman Scherer <roman@burningswell.com>
2014-11-10 09:13:29 +0100

    Remove magit customization

--- custom.el
+++ custom.el
@@ -3,10 +3,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(cider-test-report-on-success nil)
  '(column-number-mode t)
- '(custom-safe-themes (quote ("8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
  '(display-time-mode t)
- '(safe-local-variable-values (quote ((encoding . binary))))
+ '(safe-local-variable-values (quote ((emacs-lisp-docstring-fill-column . 75) (encoding . binary))))
  '(show-paren-mode t)
  '(tool-bar-mode nil))
 (custom-set-faces
@@ -14,6 +15,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Inconsolata" :foundry "unknown" :slant normal :weight normal :height 143 :width normal))))
  '(company-preview ((t (:foreground "darkgray" :underline t))))
  '(company-preview-common ((t (:inherit company-preview))))
  '(company-scrollbar-bg ((t (:background "darkgray"))))
@@ -23,8 +25,6 @@
  '(company-tooltip-common-selection ((((type x)) (:inherit company-tooltip-selection :weight bold)) (t (:inherit company-tooltip-selection))))
  '(company-tooltip-selection ((t (:background "steelblue" :foreground "white"))))
  '(cursor ((t (:background "orange red" :foreground "white"))))
- '(magit-header ((t (:foreground "gainsboro"))))
- '(magit-section-title ((t (:underline t))))
  '(mode-line ((t (:background "white smoke" :foreground "black"))))
  '(mode-line-inactive ((t (:background "dark gray" :foreground "black"))))
  '(vertical-border ((t (:foreground "black")))))
