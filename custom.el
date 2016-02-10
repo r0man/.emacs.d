5de03b3209c75a2ffde561a3d99f35e29d4184db
Roman Scherer <roman@burningswell.com>
2016-02-10 12:06:27 +0100

    Cleanup customizations

--- custom.el
+++ custom.el
@@ -3,19 +3,23 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(cider-test-report-on-success nil)
+ '(background-color "#141b23")
+ '(background-mode dark)
  '(column-number-mode t)
- '(custom-safe-themes (quote ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" default)))
+ '(cursor-color "#839496")
  '(display-time-mode t)
- '(safe-local-variable-values (quote ((emacs-lisp-docstring-fill-column . 75) (encoding . binary))))
- '(show-paren-mode t)
- '(tool-bar-mode nil))
+ '(foreground-color "#839496")
+ '(safe-local-variable-values
+   (quote
+    ((emacs-lisp-docstring-fill-column . 75)
+     (encoding . binary)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(default ((t (:family "Inconsolata" :foundry "unknown" :slant normal :weight normal :height 143 :width normal))))
+ '(Man-overstrike ((t (:foreground "#d33682" :background "#141b23" :inverse-video nil))))
  '(company-preview ((t (:foreground "darkgray" :underline t))))
  '(company-preview-common ((t (:inherit company-preview))))
  '(company-scrollbar-bg ((t (:background "darkgray"))))
@@ -25,6 +29,8 @@
  '(company-tooltip-common-selection ((((type x)) (:inherit company-tooltip-selection :weight bold)) (t (:inherit company-tooltip-selection))))
  '(company-tooltip-selection ((t (:background "steelblue" :foreground "white"))))
  '(cursor ((t (:background "orange red" :foreground "white"))))
+ '(ediff-current-diff-C ((t (:background "black"))))
+ '(ert-test-result-expected ((t (:background "green3" :foreground "white smoke"))))
  '(mode-line ((t (:background "white smoke" :foreground "black"))))
  '(mode-line-inactive ((t (:background "dark gray" :foreground "black"))))
  '(vertical-border ((t (:foreground "black")))))
