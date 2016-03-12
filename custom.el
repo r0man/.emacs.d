c2b2f961b1d26ac4d7ac2a381730b9dd5bd36dd2
Roman Scherer <roman@burningswell.com>
2016-03-12 11:47:39 +0100

    Customizations

--- custom.el
+++ custom.el
@@ -5,8 +5,13 @@
  ;; If there is more than one, they won't work right.
  '(background-color "#141b23")
  '(background-mode dark)
+ '(cider-cljs-lein-repl
+   "(do (require 'cljs.repl.node) (cemerick.piggieback/cljs-repl (cljs.repl.node/repl-env)))")
  '(column-number-mode t)
  '(cursor-color "#839496")
+ '(custom-safe-themes
+   (quote
+    ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default)))
  '(display-time-mode t)
  '(foreground-color "#839496")
  '(helm-M-x-fuzzy-match t)
@@ -14,7 +19,9 @@
  '(helm-mode t)
  '(safe-local-variable-values
    (quote
-    ((emacs-lisp-docstring-fill-column . 75)
+    ((bug-reference-bug-regexp . "#\\(?2:[[:digit:]]+\\)")
+     (cider-cljs-lein-repl . "(do (use 'figwheel-sidecar.repl-api) (start-figwheel!) (cljs-repl))")
+     (emacs-lisp-docstring-fill-column . 75)
      (encoding . binary)))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
