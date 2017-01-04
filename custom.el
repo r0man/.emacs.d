a2ee1b69fb2dc87a509efd1b72a1fd485bedd182
r0man <roman@burningswell.com>
2017-01-04 12:57:39 +0100

    Use ido instead of helm

--- custom.el
+++ custom.el
@@ -14,9 +14,6 @@
     ("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default)))
  '(display-time-mode t)
  '(foreground-color "#839496")
- '(helm-M-x-fuzzy-match t)
- '(helm-completion-in-region-fuzzy-match t)
- '(helm-mode t)
  '(safe-local-variable-values
    (quote
     ((bug-reference-bug-regexp . "#\\(?2:[[:digit:]]+\\)")
@@ -41,9 +38,6 @@
  '(cursor ((t (:background "orange red" :foreground "white"))))
  '(ediff-current-diff-C ((t (:background "black"))))
  '(ert-test-result-expected ((t (:background "green3" :foreground "white smoke"))))
- '(helm-M-x-key ((t (:foreground "#839496" :underline t :weight bold))))
- '(helm-selection ((t (:background "#586e75" :foreground "white smoke"))))
- '(helm-source-header ((t (:foreground "white smoke" :weight bold :height 1.1 :family "Sans Serif"))))
  '(mode-line ((t (:background "white smoke" :foreground "black"))))
  '(mode-line-inactive ((t (:background "dark gray" :foreground "black"))))
  '(vertical-border ((t (:foreground "black")))))
--- init.el
+++ init.el
@@ -4,6 +4,13 @@
 ;; embedded in literate Org-mode files.
 
 ;; Load up Org Mode and (now included) Org Babel for elisp embedded in Org Mode files
+
+;; Added by Package.el.  This must come before configurations of
+;; installed packages.  Don't delete this line.  If you don't want it,
+;; just comment it out by adding a semicolon to the start of the line.
+;; You may delete these explanatory comments.
+;; (package-initialize)
+
 (setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
 
 (let* ((org-dir (expand-file-name
