67052460711e2a8248c876cf5d8850e2a69f4ef0
r0man <roman@burningswell.com>
2023-02-26 11:53:29 +0100

    Add safe theme

--- custom.el
+++ custom.el
@@ -10,7 +10,7 @@
  '(column-number-mode t)
  '(cursor-color "#839496")
  '(custom-safe-themes
-   '("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" "8aebf25556399b58091e533e455dd50a6a9cba958cc4ebb0aab175863c25b9a4" "d677ef584c6dfc0697901a44b885cc18e206f05114c8a3b7fde674fce6180879" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" "1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" default))
+   '("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default))
  '(display-time-mode t)
  '(foreground-color "#839496")
  '(package-selected-packages
