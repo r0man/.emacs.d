e29bde2ff71fa18f975dac576dbee49b6cffe1b6
r0man <roman@burningswell.com>
2023-02-26 11:59:47 +0100

    Load customizations early

--- custom.el
+++ custom.el
@@ -10,7 +10,7 @@
  '(column-number-mode t)
  '(cursor-color "#839496")
  '(custom-safe-themes
-   '("fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default))
+   '("1e7e097ec8cb1f8c3a912d7e1e0331caeed49fef6cff220be63bd2a6ba4cc365" "fc5fcb6f1f1c1bc01305694c59a1a861b008c534cae8d0e48e4d5e81ad718bc6" default))
  '(display-time-mode t)
  '(foreground-color "#839496")
  '(package-selected-packages
