569588e50426f46f92cf2d1f50f5d3d269d79b94
Roman Scherer <roman@burningswell.com>
2024-04-17 12:15:41 +0200

    Remove contrib dirs from load path

--- init.el
+++ init.el
@@ -54,21 +54,6 @@
 
 (setq dotfiles-dir (file-name-directory (or (buffer-file-name) load-file-name)))
 
-(let* ((started-at (current-time))
-       (org-dir (expand-file-name
-                 "lisp" (expand-file-name
-                         "org" (expand-file-name
-                                "src" dotfiles-dir))))
-       (org-contrib-dir (expand-file-name
-                         "lisp" (expand-file-name
-                                 "contrib" (expand-file-name
-                                            ".." org-dir))))
-       (load-path (append (list org-dir org-contrib-dir)
-                          (or load-path nil))))
-  ;; load up Org-mode and Org-babel
-  (require 'ob-tangle)
-  (message "Loaded Org Mode (%.03fs)" (float-time (time-since started-at))))
-
 ;; Load up all literate org-mode files in this directory
 (mapc (lambda (filename)
         (let ((started-at (current-time)))
