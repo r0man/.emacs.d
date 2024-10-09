abe0942f61fa80845db53574b81343abe6001116
Roman Scherer <roman@burningswell.com>
2024-10-09 19:40:51 +0200

    Disable ui early

--- init.el
+++ init.el
@@ -6,6 +6,14 @@
 
 ;;; Code:
 
+(add-to-list 'default-frame-alist '(horizontal-scroll-bars . nil))
+(add-to-list 'default-frame-alist '(vertical-scroll-bars . nil))
+
+(if (fboundp 'horizontal-scroll-bar-mode) (horizontal-scroll-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+
 ;; Load customization settings first.
 
 (setq custom-file (expand-file-name "~/.emacs.d/custom.el"))
