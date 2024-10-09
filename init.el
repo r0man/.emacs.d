7d08ea58a655dcbfc093e2cd9140dabfe1067550
Roman Scherer <roman@burningswell.com>
2024-10-09 09:25:24 +0200

    Disable scroll bars in new frames

--- init.el
+++ init.el
@@ -6,13 +6,6 @@
 
 ;;; Code:
 
-;; Disable menu, scroll and tool bars.
-
-(if (fboundp 'horizontal-scroll-bar-mode) (horizontal-scroll-bar-mode -1))
-(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
-(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
-(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
-
 ;; Load customization settings first.
 
 (setq custom-file (expand-file-name "~/.emacs.d/custom.el"))
